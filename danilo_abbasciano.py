3ff7e00fef634080f123a2ccd251ff1bae5bdda7
Danilo Abbasciano <danilo.abbasciano@babel.it>
2016-04-17 11:27:21 +0200

    add sleep in a while

--- danilo_abbasciano.py
+++ danilo_abbasciano.py
@@ -9,12 +9,14 @@ INPUT_CODE=''
 while (len(INPUT_CODE) < 4):
     if button_a.is_pressed():
         INPUT_CODE = INPUT_CODE + 'A'
-    if button_b.is_pressed():
+    elif button_b.is_pressed():
        INPUT_CODE = INPUT_CODE + 'B'
 
+    sleep(50)
+     
 display.clear()
 
-if (SECRET_CODE == INPUT_CODE):
+if SECRET_CODE == INPUT_CODE:
     display.show(Image.HAPPY)
 else:
     display.show(Image.SAD)
