35b83d5b9567fd23b5c3e5f3622a09476d2d0c98
Danilo Abbasciano <danilo.abbasciano@babel.it>
2016-04-17 11:11:10 +0200

    fix import

--- giovanni_mancuso.py
+++ giovanni_mancuso.py
@@ -1,8 +1,10 @@
 from microbit import *
+import random
 
 display.scroll('Roll the dice!')
 while True:
     if accelerometer.was_gesture("shake"):
+        display.clear()
         display.show(str(random.randint(1, 6)))
     sleep(20)
     
